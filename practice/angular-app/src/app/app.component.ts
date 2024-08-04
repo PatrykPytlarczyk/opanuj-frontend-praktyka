2973ca423d7e855b0aeb65df2aba6f6dda50a36a
ppytlarczyk <ppytlarczyk@o4b.pl>
2024-08-04 14:31:26 +0200

    exercise one - accessible switch and accessibility

--- practice/angular-app/src/app/app.component.ts
+++ practice/angular-app/src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwitchComponent } from './components/switch/switch.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { FormInputComponent } from './components/form-input/form-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwitchComponent, ReactiveFormsModule, NgIf, FormInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-app';
+  form: FormGroup;
+  consentControl: FormControl;
+  firstNameControl: FormControl;
+  lastNameControl: FormControl;
+  emailControl: FormControl;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.setControls();
+    this.setForm();
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted', this.form.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  private setForm() {
+    this.form = this.fb.group({
+      firstName: this.firstNameControl,
+      lastName: this.lastNameControl,
+      email: this.emailControl,
+      consent: this.consentControl
+    });
+  }
+
+  private setControls() {
+    this.consentControl = new FormControl(false, Validators.requiredTrue);
+    this.firstNameControl = new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z]+$')]);
+    this.lastNameControl = new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z]+$')]);
+    this.emailControl = new FormControl('', [Validators.required, Validators.email]);
+  }
 }
